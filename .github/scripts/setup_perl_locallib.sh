3aa762bc8c41067634301e182c15f245c3a86a1e
Håkon Hægland <hakon.hagland@gmail.com>
2021-01-28 21:24:33 +0100

    Auto commit: Thu Jan 28 21:24:33 2021

--- .github/scripts/setup_perl_locallib.sh
+++ .github/scripts/setup_perl_locallib.sh
@@ -1,7 +1,14 @@
 #! /bin/bash
-
+tempdir=$(mktemp -d)
+cd $tempdir
+wget https://cpan.metacpan.org/authors/id/H/HA/HAARG/local-lib-2.000024.tar.gz
+tar zxvf local-lib-2.000024.tar.gz
+cd local-lib-2.000024
 PERL_LOCAL_LIB_ROOT=$GITHUB_WORKSPACE/perl5
-cpan -Mlocal::lib=$PERL_LOCAL_LIB_ROOT
+perl Makefile.PL --bootstrap=$PERL_LOCAL_LIB_ROOT
+make
+make test
+#cpan -Mlocal::lib=$PERL_LOCAL_LIB_ROOT
 echo "PERL_LOCAL_LIB_ROOT=$PERL_LOCAL_LIB_ROOT" >> $GITHUB_ENV
 echo "PERL5LIB=$PERL_LOCAL_LIB_ROOT/lib/perl5" >> $GITHUB_ENV
 echo "PERL_MB_OPT=--install_base \"$PERL_LOCAL_LIB_ROOT/\"" >> $GITHUB_ENV
