cf2505964c60a5742e6663394144164f59a4c63a
Håkon Hægland <hakon.hagland@gmail.com>
2021-01-29 16:44:30 +0100

    Auto commit: Fri Jan 29 16:44:30 2021

--- .github/scripts/install_dependencies.sh
+++ .github/scripts/install_dependencies.sh
@@ -1,15 +1,7 @@
 #! /bin/bash
-echo "Permissions /usr/local:"
-ls -l /usr/local
-echo "Permissions PERL_LOCAL_LIB_ROOT:"
-ls -l $PERL_LOCAL_LIB_ROOT
-echo "Permissions bin:"
-ls -ld $PERL_LOCAL_LIB_ROOT/bin
-echo "Content of bin:"
-ls -l $PERL_LOCAL_LIB_ROOT/bin
-echo "which cpanm"
-which cpanm
 
+export PATH PERL_LOCAL_LIB_ROOT PERL_MB_OPT PERL_MM_OPT PERL5LIB
+which cpanm
 CPANM=cpanm
 if [[ $GITHUB_WORKFLOW == *"windows"* ]] ; then
     #CPANM="perl -S cpanm"
