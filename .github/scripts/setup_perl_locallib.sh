c3d77a76148053b9bdeb4582d0fef8f1eb97934c
Håkon Hægland <hakon.hagland@gmail.com>
2021-01-29 11:20:56 +0100

    Auto commit: Fri Jan 29 11:20:56 2021

--- .github/scripts/setup_perl_locallib.sh
+++ .github/scripts/setup_perl_locallib.sh
@@ -1,21 +1,12 @@
 #! /bin/bash
 
-tempdir=$(mktemp -d)
-cd $tempdir
-wget https://cpan.metacpan.org/authors/id/H/HA/HAARG/local-lib-2.000024.tar.gz
-tar zxvf local-lib-2.000024.tar.gz
-cd local-lib-2.000024
 TOP=$GITHUB_WORKSPACE
 if [[ $GITHUB_WORKFLOW == *"windows"* ]] ; then
     TOP=$(cygpath "$TOP")
 fi
-PERL_LOCAL_LIB_ROOT=$TOP/perl5
-perl Makefile.PL --bootstrap=$PERL_LOCAL_LIB_ROOT
-make
-make test
-##cpan -Mlocal::lib=$PERL_LOCAL_LIB_ROOT
-echo "PERL_LOCAL_LIB_ROOT=$PERL_LOCAL_LIB_ROOT" >> $GITHUB_ENV
-echo "PERL5LIB=$PERL_LOCAL_LIB_ROOT/lib/perl5" >> $GITHUB_ENV
-echo "PERL_MB_OPT=--install_base \"$PERL_LOCAL_LIB_ROOT/\"" >> $GITHUB_ENV
-echo "PERL_MM_OPT=INSTALL_BASE=$PERL_LOCAL_LIB_ROOT" >> $GITHUB_ENV
-echo "PATH=$PERL_LOCAL_LIB_ROOT/bin:$PATH" >> $GITHUB_ENV
+PERL_LOCAL_LIB_ROOT="$TOP/perl5"
+mkdir -p "$PERL_LOCAL_LIB_ROOT"
+>>"$GITHUB_ENV" echo "PERL5LIB=$PERL_LOCAL_LIB_ROOT/lib/perl5"
+>>"$GITHUB_ENV" echo "PERL_MB_OPT=--install_base \"$PERL_LOCAL_LIB_ROOT/\""
+>>"$GITHUB_ENV" echo "PERL_MM_OPT=INSTALL_BASE=$PERL_LOCAL_LIB_ROOT"
+>>"$GITHUB_ENV" echo "PATH=$PERL_LOCAL_LIB_ROOT/bin:$PATH"
