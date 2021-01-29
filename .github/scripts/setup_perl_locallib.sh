10aed00992d1d748102dff509cc3e5a4574f3c55
Håkon Hægland <hakon.hagland@gmail.com>
2021-01-29 11:35:51 +0100

    Auto commit: Fri Jan 29 11:35:51 2021

--- .github/scripts/setup_perl_locallib.sh
+++ .github/scripts/setup_perl_locallib.sh
@@ -7,6 +7,13 @@ if [[ $GITHUB_WORKFLOW == *"windows"* ]] ; then
 fi
 PERL_LOCAL_LIB_ROOT="$TOP/perl5"
 mkdir -p "$PERL_LOCAL_LIB_ROOT"
+echo "Permissions HOME:"
+ls -l $HOME
+echo "Permissions PERL_LOCAL_LIB_ROOT:"
+ls -l $PERL_LOCAL_LIB_ROOT
+echo "Permissions bin:"
+ls -ld $PERL_LOCAL_LIB_ROOT/bin
+
 >>"$GITHUB_ENV" echo "PERL5LIB=$PERL_LOCAL_LIB_ROOT/lib/perl5"
 >>"$GITHUB_ENV" echo "PERL_MB_OPT=--install_base \"$PERL_LOCAL_LIB_ROOT/\""
 >>"$GITHUB_ENV" echo "PERL_MM_OPT=INSTALL_BASE=$PERL_LOCAL_LIB_ROOT"
