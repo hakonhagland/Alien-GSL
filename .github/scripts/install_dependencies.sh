b8f6fbfc7ec82502b6fbf607771bbf08f832dd83
Håkon Hægland <hakon.hagland@gmail.com>
2021-01-29 21:59:43 +0100

    Auto commit: Fri Jan 29 21:59:43 2021

--- .github/scripts/install_dependencies.sh
+++ .github/scripts/install_dependencies.sh
@@ -2,8 +2,15 @@
 
 CPANM=cpanm
 if [[ $GITHUB_WORKFLOW == *"windows"* ]] ; then
-    #CPANM="perl -S cpanm"
+    # NOTE: MSYS2 shell does not recognize the new PATH, see
+    #      https://github.com/msys2/setup-msys2/issues/106
+    #   so we will use an absolute path on MSYS2
     CPANM="$PERL_LOCAL_LIB_ROOT"/bin/cpanm
 fi
-$CPANM -v Alien::Build
+# NOTE: Alien Build currently has test failures, see
+# - https://github.com/PerlAlien/Alien-Build/pull/247
+# - https://github.com/PerlAlien/Alien-Build/pull/246
+# - https://github.com/PerlAlien/Alien-Build/pull/245
+# So we will install without testing until the above pull request has been merged.
+$CPANM -n Alien::Build
 $CPANM -v --installdeps .
